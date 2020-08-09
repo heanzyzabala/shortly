71b3ab1e1515400b25611f4ef87c907f19b95d19
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-08-09 21:49:23 +0800

    Removed unused eslint exception

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CustomForm from './components/CustomForm';
 import CustomFooter from './components/CustomFooter';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default function App() {
     return (
         <>

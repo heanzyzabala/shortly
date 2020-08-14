8c8b0c834d3ca6acd6bd9cc4999260b9f9c17d5c
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-08-14 20:42:11 +0800

    Removed footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import CustomForm from './components/CustomForm';
-import CustomFooter from './components/CustomFooter';
 
 export default function App() {
     return (
         <>
             <CustomForm />
-            <CustomFooter />
         </>
     );
 }
